66c1da892d5a238348eaefbe21f89e772771f972
StoneShiShang <43474457+Stoneshishang@users.noreply.github.com>
2021-01-06 17:34:11 -0500

    initial commit

--- util.js
+++ util.js
@@ -1,6 +1,7 @@
 exports.generateText = (name, age) => {
   // Returns output text
   return `${name} (${age} years old)`;
+  // return 'Shang (29 years old)';
 };
 
 exports.createElement = (type, text, className) => {
