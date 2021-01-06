78b96edf2a1fcac837ec4cce6501865d76c2b2d2
StoneShiShang <43474457+Stoneshishang@users.noreply.github.com>
2021-01-06 17:44:22 -0500

    check for false postive

--- util.test.js
+++ util.test.js
@@ -3,9 +3,11 @@ const { generateText } = require('./util');
 test('should output name and age', ()=>{
   const text = generateText('Shang', 29);
   expect(text).toBe('Shang (29 years old)')
+  const text2 = generateText('Anna', 28);
+  expect(text2).toBe('Anna (28 years old)');
 })
 
 test('should output data-less text', ()=>{
-  const text =  generateText('', null);
-  expect(text).toBe(' (null years old)');
+  const text =  generateText();
+  expect(text).toBe('undefined (undefined years old)');
 })
