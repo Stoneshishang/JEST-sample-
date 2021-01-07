4eb74f0bf66d6395727554d42f1b6e1f79fd89bb
StoneShiShang <43474457+Stoneshishang@users.noreply.github.com>
2021-01-07 09:41:01 -0500

    intergration test completed by combining the unit tests.

--- util.test.js
+++ util.test.js
@@ -1,13 +1,12 @@
-const { generateText } = require('./util');
+const { generateText, checkAndGenerate } = require('./util');
 
 test('should output name and age', ()=>{
   const text = generateText('Shang', 29);
   expect(text).toBe('Shang (29 years old)')
-  const text2 = generateText('Anna', 28);
-  expect(text2).toBe('Anna (28 years old)');
+  
 })
 
-test('should output data-less text', ()=>{
-  const text =  generateText();
-  expect(text).toBe('undefined (undefined years old)');
-})
+test('should generate a valid text output', ()=>{
+  const text = checkAndGenerate('Shang',29);
+  expect(text).toBe('Shang (29 years old)')
+});
